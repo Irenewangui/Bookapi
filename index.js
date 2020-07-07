6014cf734dc22400b012011faec82a3534880cd7
Irene Wangui <i.wangui@medluxinternational.com>
2020-07-07 21:26:37 +0300

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 var bookRouter = express.Router();
 var Book = require ("./models/bookModel");
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 bookRouter.route ("/books")
     .get( function( req, res ){
@@ -26,7 +26,6 @@ bookRouter.route ("/books")
         
     })
     .post(function(req, res){
-        console.log(req.body);
         if (!req.body.title){
             res.status(404);
             res.send("title not provided")
@@ -66,40 +65,58 @@ bookRouter.route ("/books/:bookId")
         })
         
     })
-    .delete( function(req, res ){
+    .delete(function(req, res ){
         
-        Book.findById(req.params.bookId, function(err, book){
-           book.remove(function(){
-             if (error)
-                res.status(500).send(error);
-             else 
-                res.status(204).send("book has been removed");
-
-           })
-                
-                
+        Book.findById(req.params.bookId, function(error, book){
+           if(!book){
+               res.status(404).send("Book does not exist ");
+           }
+           else{
+            book.remove(function(){
+                if (error)
+                   res.status(500).send(error + "book does not exist");
+                else 
+                   res.status(204).send("book has been removed");
+   
+              })
+           }
+            
         })  
         
+   
+    })
+    .patch(function(req, res ){
+        if (req.body._id)
+            delete req.body._id
+        Book.findById(req.params.bookId, function(err, book){
+            for(var item in req.body){
+                book[item] = req.body[item]
+            }
+            book.save();
+            res.send("Book has been updated.");
+            
+        })  
+        
+   
     })
 
+//acts as a middleware
+//to handle CORS Errors
+app.use((req, res, next) => { //doesn't send response just adjusts it
+    res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, WANYUGE, PATCH, DELETE, GET'); //to give access to all the methods provided
+        return res.status(200).json({});
+    }
+    next(); //so that other routes can take over
+})
 
 app.use("/api", bookRouter)
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(port, function(){
     console.log("server running on port: " + port);
 });
+
